74e1be253077823ea8fb04e47770917a81f8a7f5
andrewcannan <>
2023-06-05 20:00:04 +0000

    adjust validation function to check values are intergers

--- run.py
+++ run.py
@@ -32,10 +32,11 @@ def validate_data(values):
     or if there are not exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f'Exactly 6 values required, you provided {len(values)}'
                 )
     except ValueError as e:
-        print(f'Invalid data {e}, please try again.\n')
+        print(f'Invalid data: {e}, please try again.\n')
 get_sales_data()
